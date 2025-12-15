06d98061824a7ed20174720375f39a4596f14443
Nikita Bizyuk <bizyuknikita97@gmail.com>
2025-12-15 02:18:33 -0800

    same tree and balanced tree (python)

---
+++ Python/trees/same_tree.py
@@ -0,0 +1,11 @@
+from TreeNode import TreeNode
+from typing import Optional
+
+class Solution:
+
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or q.val != p.val:
+            return False
+        return (self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right))
